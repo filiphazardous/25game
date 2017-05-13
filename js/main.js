2248d5ef353d8b1bf1af635c5577e750e9357eb9
Filip Joelsson <filip@pixiecoder.com>
2017-05-13 18:54:17 +0200

    Added events for drag and drop

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ require(['hazUtils.js/general/debug', 'hazUtils.js/web/dom/elements'], function 
     var scoreDisplay = elm.get('#score');
     var gameTiles = elm.get('.game-tile');
     var gameBoardWidth = 5;
+    var draggedTile = null;
+    var dropTiles = [];
 
     var gameStats = {
         running: false,
-        score: 0,
+        score: 0
     };
 
     elm.addEvent(startButton, 'click', gameStart);
@@ -17,12 +19,15 @@ require(['hazUtils.js/general/debug', 'hazUtils.js/web/dom/elements'], function 
 
     function gameStart() {
 
-        gameStats.score = 0;
-        gameStats.running = true;
-
         scrambleElements();
 
-        elm.setAttr(gameTiles, 'draggable', true);
+        if (gameStats.running === false) {
+            elm.setAttr(gameTiles, 'draggable', true);
+            elm.addEvent(gameTiles, 'dragstart', onDragStart);
+        }
+
+        gameStats.score = 0;
+        gameStats.running = true;
 
         updateScore();
     }
@@ -31,16 +36,69 @@ require(['hazUtils.js/general/debug', 'hazUtils.js/web/dom/elements'], function 
 
         // Remove dragability on all elements
         elm.remAttr(gameTiles, 'draggable');
+        elm.remEvent(gameTiles, 'dragstart', onDragStart);
     }
 
-    function onDragStart() {
+    function onDragStart(evt) {
 
-        // Enable dropability on elements on the same x- and y-axes
+        if (draggedTile) {
+            debug.log("Error: A tile is already being dragged!\n", draggedTile);
+            return;
+        }
+        // Enable dropability on elements on the same x- and y-axes and change their color?
+        debug.log("onDragStart\n", evt);
+        draggedTile = evt.target ? evt.target : evt.srcElement;
+        debug.log(draggedTile.data);
+
+        elm.addEvent(draggedTile, 'dragend', onDragEnd);
+
+        for (var i = gameTiles.length - 1; i >= 0; --i) {
+            if (gameTiles[i].data.index === draggedTile.data.index) continue;
+
+            if (gameTiles[i].data.col === draggedTile.data.col
+                || gameTiles[i].data.row === draggedTile.data.row) {
+                dropTiles.push(gameTiles[i]);
+            }
+        }
+
+        elm.addEvent(dropTiles, 'dragover', onDragOver);
+        elm.addEvent(dropTiles, 'drop', onDrop);
+        elm.addClasses(dropTiles, 'drop-zone');
     }
 
-    function onDragStop() {
+    function onDragEnd(evt) {
 
-        // Update positions and
+        debug.log("onDragEnd called\n", evt);
+
+        elm.remEvent(dropTiles, 'dragover', onDragOver);
+        elm.remEvent(dropTiles, 'drop', onDragStart);
+        elm.remClasses(dropTiles, 'drop-zone');
+        dropTiles = [];
+
+        elm.remEvent(draggedTile, 'dragend', onDragEnd);
+        draggedTile = null;
+    }
+
+    function onDragOver(evt) {
+
+        // Don't do much, maybe change color?
+        evt.preventDefault();
+    }
+
+    function onDrop(evt) {
+
+        // Update positions, remove dropability, and change colors back
+        evt.preventDefault();
+        debug.log("onDrop\n", evt);
+
+
+
+        updateElementPos();
+
+        ++gameStats.score;
+        updateScore();
+
+        setTimeout(checkIfComplete, 200);
     }
 
     function checkIfComplete() {
@@ -55,6 +113,7 @@ require(['hazUtils.js/general/debug', 'hazUtils.js/web/dom/elements'], function 
         while (gameTiles.length) {
             var index = Math.floor(Math.random() * gameTiles.length);
             var tile = gameTiles.splice(index, 1)[0];
+            tile.data = {};
             newGameTiles.push(tile);
         }
 
@@ -77,6 +136,11 @@ require(['hazUtils.js/general/debug', 'hazUtils.js/web/dom/elements'], function 
             var top = (tileHeight * Math.floor(i / gameBoardWidth)) + '%';
             gameTiles[i].style.left = left;
             gameTiles[i].style.top = top;
+            gameTiles[i].data = {
+                index: i,
+                row: Math.floor(i / gameBoardWidth),
+                col: i % gameBoardWidth
+            };
         }
     }
 
